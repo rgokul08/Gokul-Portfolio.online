fabb4d49902238c678eda78de60cc3f167fe0a62
Gokul <rgokul08.in@gmail.com>
2026-06-17 22:02:13 +0530

    8.2v (edit in loader page )

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,66 +1,23 @@
-import React, { useEffect, useState } from 'react'
-import './Loader.css'
+// src/components/Loader.jsx
 
-export default function Loader() {
-  const [p, setP] = useState(0)
-  
-  useEffect(() => {
-    const iv = setInterval(() =>
-      setP(v => {
-        if (v >= 100) {
-          clearInterval(iv)
-          return 100
-        }
-        return Math.min(v + Math.random() * 14 + 3, 100)
-      }),
-      90
-    )
-    return () => clearInterval(iv)
-  }, [])
+import CosmicParallaxBg from "../components/CosmicParallaxBg";
 
+const Loader = () => {
   return (
-    <div className="loader">
-      {/* Cloud Waves Background */}
-      <div className="loader-clouds">
-        <div className="cloud cloud-1" />
-        <div className="cloud cloud-2" />
-        <div className="cloud cloud-3" />
-        <div className="cloud cloud-4" />
-        <div className="cloud cloud-5" />
-      </div>
-
-      {/* Galaxy Particles */}
-      <div className="loader-galaxy">
-        {[...Array(15)].map((_, i) => (
-          <div
-            key={i}
-            className="galaxy-particle"
-            style={{
-              '--delay': `${i * 0.12}s`,
-              '--duration': `${3.5 + i * 0.25}s`,
-              '--size': `${Math.random() * 100 + 30}px`,
-            }}
-          />
-        ))}
-      </div>
-
-      {/* Grid Background */}
-      <div className="loader-grid" />
-
-      {/* Scan Line */}
-      <div className="loader-scan" />
-
-      {/* Content */}
-      <div className="loader-inner">
-        <div className="loader-logo">
-          <span>G</span>
-        </div>
-        <div className="loader-name">GOKUL R</div>
-        <div className="loader-bar-wrap">
-          <div className="loader-bar" style={{ width: `${Math.min(p, 100)}%` }} />
-        </div>
-        <div className="loader-text">INITIALIZING PORTFOLIO…</div>
-      </div>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <CosmicParallaxBg
+        head="Gokul"
+        text="Portfolio Loading..."
+        loop={true}
+      />
     </div>
-  )
-}
+  );
+};
+
+export default Loader;
