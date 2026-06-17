678b13af154f3a38a563003297aa82c37c3334c7
Gokul <rgokul08.in@gmail.com>
2026-06-17 22:27:39 +0530

    edit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,23 +1,61 @@
-// src/components/Loader.jsx
+import React, { useEffect, useState } from 'react'
+import './Loader.css'
 
-import CosmicParallaxBg from "../components/CosmicParallaxBg";
+export default function Loader() {
+  const [smallStars, setSmallStars] = useState('')
+  const [mediumStars, setMediumStars] = useState('')
+  const [bigStars, setBigStars] = useState('')
 
-const Loader = () => {
+  const generateStarBoxShadow = count => {
+    const shadows = []
+
+    for (let i = 0; i < count; i++) {
+      const x = Math.floor(Math.random() * 2000)
+      const y = Math.floor(Math.random() * 2000)
+      shadows.push(`${x}px ${y}px #FFF`)
+    }
+
+    return shadows.join(', ')
+  }
+
+  useEffect(() => {
+    setSmallStars(generateStarBoxShadow(700))
+    setMediumStars(generateStarBoxShadow(200))
+    setBigStars(generateStarBoxShadow(100))
+  }, [])
+
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        overflow: "hidden",
-      }}
-    >
-      <CosmicParallaxBg
-        head="Gokul"
-        text="Portfolio Loading..."
-        loop={true}
+    <div className="cosmic-parallax-container">
+      <div
+        id="stars"
+        style={{ boxShadow: smallStars }}
       />
-    </div>
-  );
-};
 
-export default Loader;
+      <div
+        id="stars2"
+        style={{ boxShadow: mediumStars }}
+      />
+
+      <div
+        id="stars3"
+        style={{ boxShadow: bigStars }}
+      />
+
+      <div id="horizon">
+        <div className="glow" />
+      </div>
+
+      <div id="earth" />
+
+      <div id="title">GOKUL R</div>
+
+      <div id="subtitle">
+        <span>WEB DEVELOPER</span>
+        <span> • </span>
+        <span>UI DESIGNER</span>
+        <span> • </span>
+        <span>DATA ANALYST</span>
+      </div>
+    </div>
+  )
+}
